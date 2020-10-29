e92568e70b8f33aa96fdba68919130b11d5f2e11
dreamsyang <dreamsyang@tencent.com>
2020-10-29 16:10:06 +0800

    feat:import vue-socket.io and establish a WebSocket connection（static）

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
 import Vue from 'vue';
+import VueSocketIO from 'vue-socket.io';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 Vue.config.productionTip = false;
+
+// connection为静态地址时
+Vue.use(new VueSocketIO({
+  debug: true,
+  connection: 'http://127.0.0.1:7001/',
+}));
 
 new Vue({
   router,
